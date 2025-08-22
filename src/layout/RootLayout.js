3bbe021c30876abec8b7e1e00f460d0eadc8466c
rungrojcarrent <rungrojcarrentudon@gmail.com>
2025-08-22 17:05:50 +0700

    Update RootLayout.js

--- src/layout/RootLayout.js
+++ src/layout/RootLayout.js
@@ -30,10 +30,10 @@ export default function RootLayout() {
           boxShadow: '0 2px 12px rgba(0,0,0,0.08)',
           pointerEvents: 'auto',
         }}>
-          <a href="https://www.instagram.com/rungroj.info/" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.youtube.com/@RungrojCarRental" target="_blank" rel="noopener noreferrer">
             <img src="https://www.terminal21.co.th/korat/wp-content/uploads/2016/10/Instagram_App_Large_May2016_200.png" alt="ig_icon" width="33" height="33" />
           </a>
-          <a href="https://www.facebook.com/rungroj.co.th/" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.facebook.com/RungrojCarRental/" target="_blank" rel="noopener noreferrer">
             <img src="https://www.terminal21.co.th/korat/wp-content/uploads/2016/10/Facebook-Logo.png" alt="facebook" width="33" height="33" />
           </a>
           <a href="https://page.line.me/rungroj/?accountId=rungroj" target="_blank" rel="noopener noreferrer">
@@ -42,7 +42,7 @@ export default function RootLayout() {
           <a href="https://twitter.com/rungrojcarrentudon" target="_blank" rel="noopener noreferrer">
             <img src="https://www.terminal21.co.th/korat/wp-content/uploads/2024/01/X-twitter_logo.png" alt="twitter" width="33" height="33" />
           </a>
-          <a href="https://www.tiktok.com/@rungrojcarrent1" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.tiktok.com/@RungrojCarRental" target="_blank" rel="noopener noreferrer">
             <img src="https://www.terminal21.co.th/korat/wp-content/uploads/2022/02/tictok-logo-square.png" alt="tiktok" width="33" height="33" />
           </a>
           <span style={{marginLeft: '18px', color: '#222', fontSize: '0.98rem', fontWeight: 500}}>
